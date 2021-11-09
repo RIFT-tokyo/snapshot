781e7cef566a7ca3d6e391a57a84ad886a138072
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-09 21:57:44 +0900

    feat: CSSでおしゃれにしました

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Form from './Form';
+import Navigation from './Navigation';
 
 const Header: React.VFC<{handleSubmit: any }> = ({ handleSubmit }) => {
 	return (
 		<div>
 			<h1>SnapShot</h1>
-			<Form handleSubmit={handleSubmit}/>
+			<Form handleSubmit={handleSubmit} />
+			<Navigation />
 		</div>
 	)
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+const Navigation: React.VFC = () => {
+	return (
+		<nav className="main-nav">
+			<ul>
+				<li>Mountain</li>
+				<li>Beaches</li>
+				<li>Birds</li>
+				<li>Food</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navigation;
